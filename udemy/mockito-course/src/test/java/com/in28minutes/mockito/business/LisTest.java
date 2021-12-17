3ddf0956de8d141a4e70a111f8acaef2a7a0a225
yanJuicy <hanjub3@gmail.com>
2021-12-17 17:49:45 +0900

    Mockito를 이용한 다양한 예

--- udemy/mockito-course/src/test/java/com/in28minutes/mockito/business/LisTest.java
+++ udemy/mockito-course/src/test/java/com/in28minutes/mockito/business/LisTest.java
@@ -1,5 +1,6 @@
 package com.in28minutes.mockito.business;
 
+import static org.hamcrest.CoreMatchers.any;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -23,6 +24,24 @@ public class LisTest {
 		when(listMock.getItemCount()).thenReturn(2).thenReturn(3);
 		assertEquals(2, listMock.getItemCount());
 		assertEquals(3, listMock.getItemCount());
+	}
+	
+	@Test
+	public void letsMockListGet() {
+		List listMock = mock(List.class);
+		when(listMock.getItem(0)).thenReturn("in28Minutes");
+		
+		assertEquals("in28Minutes", listMock.getItem(0));
+		assertEquals(null, listMock.getItem(1));
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void letsMockList_throwAnExcpetion() {
+		List listMock = mock(List.class);
+		
+		when(listMock.getItem(0)).thenThrow(new RuntimeException("Something"));
+		
+		listMock.getItem(0);
 	}
 	
 }
