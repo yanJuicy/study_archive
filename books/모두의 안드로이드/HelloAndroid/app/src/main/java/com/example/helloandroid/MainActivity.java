92c066455a99d09e888cd27a0d956fd096293f27
Yanjoo <hanjub3@gmail.com>
2022-01-16 19:56:23 +0900

    HelloAndroid

---
+++ books/모두의 안드로이드/HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloandroid;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        TextView tv = findViewById(R.id.textView);
+        tv.setText("Hi!");
+    }
+}
