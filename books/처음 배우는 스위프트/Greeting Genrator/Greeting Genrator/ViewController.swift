43c54256c308f9dccf9f6ea9d497924ed8330f2a
yanJuicy <hanjub3@gmail.com>
2022-01-19 00:55:37 +0900

    Greeting Genereator

---
+++ books/처음 배우는 스위프트/Greeting Genrator/Greeting Genrator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Greeting Genrator
+//
+//  Created by 이한주 on 2022/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
