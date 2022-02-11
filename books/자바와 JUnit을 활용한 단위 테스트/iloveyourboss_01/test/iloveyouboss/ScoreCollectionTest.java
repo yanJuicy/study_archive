bcc11082f8256b56d82bd8344f6b2257967028a8
yanJuicy <hanjub3@gmail.com>
2022-02-12 05:15:50 +0900

    JUnit의 기본

---
+++ books/자바와 JUnit을 활용한 단위 테스트/iloveyourboss_01/test/iloveyouboss/ScoreCollectionTest.java
@@ -0,0 +1,13 @@
+package iloveyouboss;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScoreCollectionTest {
+
+    @Test
+    public void test() {
+//        fail("Not yet implemented");
+    }
+}
