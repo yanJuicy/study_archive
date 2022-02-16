f1e918db819bcf3a4f41444b5b685e5a39a433e9
yanJuicy <hanjub3@gmail.com>
2022-02-16 17:29:22 +0900

    refactor

--- sparta_coding_club/springcore/src/main/java/com/sparta/springcore/dto/FolderCreateRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.springcore.dto;
-
-import lombok.Getter;
-import java.util.List;
-
-@Getter
-public class FolderCreateRequestDto {
-    List<String> folderNames;
-}
--- sparta_coding_club/springcore/src/main/java/com/sparta/springcore/repository/UserTimeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.springcore.repository;
-
-import com.sparta.springcore.model.User;
-import com.sparta.springcore.model.UserTime;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTimeRepository extends JpaRepository<UserTime, Long> {
-    UserTime findByUser(User user);
-}
